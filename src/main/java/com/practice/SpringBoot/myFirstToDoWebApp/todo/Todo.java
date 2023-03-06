a8e865e1f8de73c866f8e79bd2c2709ce545e0bd
ravinderRawat <rawatravinder115@gmail.com>
2023-03-06 23:10:41 +0530

    add custom security Filter chain for h2 database

--- src/main/java/com/practice/SpringBoot/myFirstToDoWebApp/todo/Todo.java
+++ src/main/java/com/practice/SpringBoot/myFirstToDoWebApp/todo/Todo.java
@@ -1,11 +1,19 @@
 package com.practice.SpringBoot.myFirstToDoWebApp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue
     private int id;
+    @Column(name="name")
     private String username;
 
     @Size(min = 10,message = "Enter Atleast 10 Character")
